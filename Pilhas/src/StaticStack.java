762ad99e9e04265653207238c64a2a1dc5725ca3
AleDominski2 <160612436+AleDominski2@users.noreply.github.com>
2024-08-13 19:39:48 -0300

    Add files via upload

---
+++ Pilhas/src/StaticStack.java
@@ -0,0 +1,50 @@
+public class StaticStack<T> {
+    private int top = -1;
+    private T[] data;
+
+    public StaticStack(int capacity) {
+        data = (T[]) new Object[capacity];
+    }
+
+    //empilhar um elemento
+    public void push(T element) {
+        if (isFull()) {
+            System.out.println("A pilha está cheia :D.");
+        } else {
+            data[++top] = element;
+            System.out.println("Elemento " + element + " empilhado.");
+        }
+    }
+
+    // desempilha um elemento
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            T elemento = data[top];
+            data[top--] = null;
+            System.out.println("Elemento " + elemento + " desempilhado.");
+            return elemento;
+        }
+    }
+
+    // limpa a pilha
+    public void clear() {
+        for (int i = 0; i <= top; i++) {
+            data[i] = null;
+        }
+        top = -1;
+        System.out.println("A pilha foi esvaziada.");
+    }
+
+    //verifica se a pilha está cheia
+    public boolean isFull() {
+        return top == data.length - 1;
+
+    }
+
+    //verifica se a pilha está vazia
+    public boolean isEmpty() {
+        return top == -1;
+    }
+}
